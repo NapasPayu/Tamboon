c1e888e8284d41282d3b4707dd668fdc0da49cac
Napas Pattamasattayasonthi <napas.psonthi@hotmail.com>
2021-03-17 08:59:37 +0700

    init coil image loader

--- app/src/main/java/com/napas/tamboon/App.kt
+++ app/src/main/java/com/napas/tamboon/App.kt
@@ -1,12 +1,17 @@
 package com.napas.tamboon
 
 import android.app.Application
+import android.os.Build
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import com.napas.tamboon.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-open class App : Application() {
+open class App : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -15,4 +20,16 @@ open class App : Application() {
             modules(appModule)
         }
     }
+
+    override fun newImageLoader(): ImageLoader =
+        ImageLoader.Builder(this)
+            .crossfade(true)
+            .componentRegistry {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder())
+                } else {
+                    add(GifDecoder())
+                }
+            }
+            .build()
 }
