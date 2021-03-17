4a5db44f79b8cde4559949fd8d432a85ee603269
Napas Pattamasattayasonthi <napas.psonthi@hotmail.com>
2021-03-17 17:00:41 +0700

    fix app bar
    to show app bar title regarding navigation label

--- app/src/main/java/com/napas/tamboon/MainActivity.kt
+++ app/src/main/java/com/napas/tamboon/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.napas.tamboon
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         navController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
 }
